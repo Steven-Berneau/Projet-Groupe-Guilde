83ec98da6f59fa607986435a010b623543918939
aurele <aurelien.plazzotta@tutanota.com>
2024-04-10 11:38:43 +0200

    Extends SQL requests with 3 new tables, adding 2 MCD for comparison and historical reasons

--- populate_db.sql
+++ populate_db.sql
@@ -1,5 +1,14 @@
+-- Clean start
 DROP database Guild;
 CREATE datable Guild;
 use Guild;
 
+-- Create tables
 create table User (id int primary key auto_increment, nickname tinytext not null, email string tinytext not null, rank tinyint(1) not null, APIKey varchar(511) not null, fname string tinytext not null, lname string tinytext not null, birthDate DATETIME not null);
+
+create table Denunciation (id int primary key auto_increment, name tinytext not null);
+create table Denunciation_type (id int primary key auto_increment, name tinytext not null);
+
+create table Event (id int primary key auto_increment, date DATETIME not null, listofAttendees text(1023), location tintytext not null, levelMinimum tinyint(255), levelMaximum tinyint(255) not null);
+
+-- Create foreign key to grant jointures capability.
