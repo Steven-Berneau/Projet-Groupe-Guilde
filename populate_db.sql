8cb58ffb5f8c0c9ee062c14a3573a07f31aa18cb
aurele <aurelien.plazzotta@tutanota.com>
2024-04-10 13:27:29 +0200

    Add first foreign key

--- populate_db.sql
+++ populate_db.sql
@@ -1,6 +1,6 @@
 -- Clean start
 DROP database Guild;
-CREATE datable Guild;
+CREATE database Guild;
 use Guild;
 
 -- Create tables
@@ -9,6 +9,18 @@ create table User (id int primary key auto_increment, nickname tinytext not null
 create table Denunciation (id int primary key auto_increment, name tinytext not null);
 create table Denunciation_type (id int primary key auto_increment, name tinytext not null);
 
-create table Event (id int primary key auto_increment, date DATETIME not null, listofAttendees text(1023), location tintytext not null, levelMinimum tinyint(255), levelMaximum tinyint(255) not null);
+create table Event (id int primary key auto_increment, date datetime not null, listofAttendees text(1023), location tintytext not null);
+
+create table Area (id int primary key auto_increment, name tinytext not null, description string tinytext not null);
+create table Instance_type (id int primary key auto_increment, description tinytext not null, levelMinimum tinyint(255) not null, levelMaximum tinyint(255) not null);
+
+create table Character (id int primary key auto_increment, archetype tinytext not null, level tinyint(255) not null);
+
+create table Rank (id int primary key auto_increment, name tinytext not null);
+
+create table Equipment (id int primary key auto_increment, name text(1023) not null);
+
+create table Message (id int primary key auto_increment, title tinytext not null, contents text(65535) not null, date datetime not null, sender tinitext not null, addressee tinytext not null);
 
 -- Create foreign key to grant jointures capability.
+alter table Area add (numDescription int, foreign key(numDescription) references Instance_type(id));
