7874937b8c784a4b6cf856fd13b6d060c961795c
aurele <aurelien.plazzotta@tutanota.com>
2024-04-10 16:22:28 +0200

    Adding accessors to Event model

--- src/models/Event.php
+++ src/models/Event.php
@@ -6,8 +6,43 @@ namespace Entities\Guild;
 
 class Event
 {
-  public function __construct(private int $id = 0, private \DateTimeImmutable $date, private $Users = new \ArrayObject(), private string $location)
+  public function __construct(private int $id = 0, private \DateTimeImmutable $date, private $users = new \ArrayObject(), private string $location, private int $levelMinimum = 1, private int $levelMaximum)
   {
     $this->id = $id;
+    $this->date = $date;
+    $this->users = $users;
+    $this->location = $location;
+    $this->levelMinimum = $levelMinimum;
+    $this->levelMaximum = $levelMaximum;
+  }
+
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  public function getDate(): \DateTimeImmutable
+  {
+    return $this->date;
+  }
+
+  public function getUsers(): \ArrayObject
+  {
+    return $this->users;
+  }
+
+  public function getLocation(): string
+  {
+    return $this->location;
+  }
+
+  public function getLevelMinimum(): int
+  {
+    return $this->levelMinimum;
+  }
+
+  public function getLevelMaximum(): int
+  {
+    return $this->levelMaximum;
   }
 }
