31593b950a819acc422238c6101a74e48cd3a21f
aurele <aurelien.plazzotta@tutanota.com>
2024-04-11 15:00:40 +0200

    Adding DB driver to map with guild_db database

---
+++ src/models/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entities\Guild;
+
+class Database
+{
+  private static ?Database $instance = null;
+
+  public function __construct(private ?\PDO $connexion = null)
+  {
+    $this->connexion = new \PDO('mysql:host=mysql-srv;dbname=guild_db', 'steven', 'stevenaurelemallo');
+  }
+
+  public static function getInstance(): Database
+  {
+    /**
+     * Singleton pattern to ensure both a single instance and a global point-access.
+     */
+    if (self::$instance == null) {
+      self::$instance = new Database();
+    }
+    return self::$instance;
+  }
+
+  public function getConnexion(): \PDO
+  {
+    return $this->connexion;
+  }
+}
