9974c5df1ba9873424c3658f127fec2c9aa7602f
Steven <steven.berneau@gmail.com>
2024-04-11 11:58:14 +0200

    Correction done

--- src/models/Denunciation.php
+++ src/models/Denunciation.php
@@ -7,23 +7,23 @@ namespace Entities\Guild;
 class Denunciation{
 
     public function __construct(private int $id = 0, private string $name){
-        $this -> _id = $id;
-        $this -> _name = $name;
+        $this -> id = $id;
+        $this -> name = $name;
     }
 
     public function getId():int{
-        return $this -> _id;
+        return $this -> id;
     }
 
     public function setId(int $id){
-        return $this -> _id = $id;
+        return $this -> id = $id;
     }
 
     public function getName():string{
-        return $this -> _name;
+        return $this -> name;
     }
 
     public function setName(string $name){
-        return $this -> _name = $name;
+        return $this -> name = $name;
     }
 }
