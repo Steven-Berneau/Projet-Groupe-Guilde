472b6a482258aec97231114983b821c67d92ff82
Steven <steven.berneau@gmail.com>
2024-04-11 09:06:32 +0200

    User class on progress

--- src/models/User.php
+++ src/models/User.php
@@ -8,5 +8,67 @@ class User{
     private int $_id;
     private string $_nickname;
     private string $_email;
-    private string $_
+    private string $_fname;
+    private string $_lname;
+    private dateImmutable $_birthdate;
+
+
+    public function __construct(int $id = 0, string $nickname, string $email, string $fname, string $lname, dateImmutable $birthdate){
+        $this -> _id = $id;
+        $this -> _nickname = $nickname;
+        $this -> _email = $email;
+        $this -> _fname = $fname;
+        $this -> _lname = $lname;
+        $this -> _birthdate = $birthdate;
+    }
+
+    public function getId():int{
+        return $this -> _id;
+    }
+
+    public function setId(int $id){
+        return $this -> _id = $id;
+    }
+
+    public function getNickname():string{
+        return $this -> _nickname;
+    }
+
+    public function setNickname(string $nickname){
+        return $this -> _nickname = $nickname;
+    }
+
+    public function getEmail():string{
+        return $this -> _email;
+    }
+
+    public function setEmail(string $email):string{
+        return $this -> _email = $email;
+    }
+
+    public function getFname():string{
+        return $this -> _fname;
+    }
+
+    public function setFname(string $fname):string{
+        return $this -> _fname = $fname;
+    }
+
+    public function getLname():string{
+        return $this -> _lname;
+    }
+
+    public function setLname(string $lname):string{
+        return $this -> _lname = $lname;
+    }
+
+    public function getBirthdate():dateImmutable{
+        return $this -> _birthdate;
+    }
+
+    public function setBirthdate(string $birthdate):dateImmutable{
+        return $this -> _birthdate = $birthdate;
+    }
+
+
 }
