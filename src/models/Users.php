1352ad29202eb0ce98c548d1641640318acb30d8
Steven <steven.berneau@gmail.com>
2024-04-12 10:10:59 +0200

    Users created

--- src/models/Users.php
+++ src/models/Users.php
@@ -10,4 +10,23 @@ class Users extends \ArrayObject
   {
     $this->users = $users;
   }
+
+  public function offsetSet($index, $newval): void
+  {
+    if (!($newval instanceof User)) {
+      throw new \InvalidArgumentException("Must be a user");
+    }
+    parent::offsetSet($index, $newval);
+  }
+
+  public static function getUsers(int $idUser): Users
+  {
+    $liste = new Users();
+    $statement = Database::getInstance()->getConnexion()->prepare('SELECT * FROM User where numUser=:numUser;');
+    $statement->execute(['numUser' => $idUser]);
+    while ($row = $statement->fetch()) {
+      $liste[] = new User(nickname: $row['nickname'], id: $row['id'], email: $row['email'], fname: $row['fname'], lname: $row['lname'], birthdate: $row['birthdate']);
+    }
+    return $liste;
+  }
 }
