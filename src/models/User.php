76070758ef1649abc0be2092af4c477166e79338
Steven <steven.berneau@gmail.com>
2024-04-15 14:44:31 +0200

    CRUD on Working

--- src/models/User.php
+++ src/models/User.php
@@ -9,7 +9,7 @@ class User
     /**
      * Simple first class User
      */
-    public function __construct(private int $id = 0, private string $nickname ="", private string $email ="", private string $fname ="", private string $lname ="", private \DateTimeImmutable $birthdate)
+    public function __construct(private int $id = 0, private string $nickname = "", private string $email = "", private string $fname = "", private string $lname = "", private \DateTimeImmutable $birthdate)
     {
         $this->id = $id;
         $this->nickname = $nickname;
@@ -77,5 +77,45 @@ class User
     public function setBirthdate(string $birthdate)
     {
         return $this->birthdate = $birthdate;
+    }
+    /**
+     * Starting CRUD
+     */
+    public static function read(int $id): ?User
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('SELECT * FROM User WHERE id=:id;');
+        $statement->execute(['id' => $id]);
+        if ($row = $statement->fetch())
+            return new User(id: $row['id'], nickname: $row['nickname'], email: $row['email'], fname: $row['fname'], lname: $row['lname'], birthdate: $row['birthdate']);
+        return NULL;
+    }
+
+    public static function create(User $user): int
+    {
+        $statement = Databse::getInstance()->getConnexion()->prepare('INSERT INTO User (nickname,email,fname,lname,birthdate) VALUES (:nickname, :email, :fname, :lname, :birthdate);');
+        $statement->execute(['nickname' => $user->getNickname(), 'email' => $user->getEmail(), 'fname' => $user->getFname(), 'lname' => $user->getLname(), 'birthdate' => $user->getBirthdate()]);
+        return (int)Database::getInstance()->getConnexion()->lastInsertionId();
+    }
+
+    public static function update(User $user)
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('UPDATE User SET  nickname=:nickname,email=:email,fname=:fname,lname=:lname,birthdate=:birthdate WHERE id=:id;');
+        $statement->execute(['nickname' => $user->getNickname(), 'email' => $user->getEmail(), 'fname' => $user->getFname(), 'lname' => $user->getLname(), 'birthdate' => $user->getBirthdate()]);
+    }
+
+    public function delete(User $user)
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('DELETE FROM User WHERE id=:id');
+        $statement->execute(['id' => $user->getId()]);
+    }
+
+    public static function loadFromJson($data)
+    {
+        foreach ($data as $u) {
+        $user = new User($u->nickname);
+        $id=User::create($user);
+        $user=User::read($id);
+        echo "id=".$id;
+        }
     }
 }
