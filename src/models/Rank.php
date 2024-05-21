4d25e5f429743765bc97c89f8b2fff8671928126
Steven <steven.berneau@gmail.com>
2024-05-21 11:16:34 +0200

    CRUD done

--- src/models/Rank.php
+++ src/models/Rank.php
@@ -11,6 +11,11 @@ class Rank
     $this->name = $name;
   }
 
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
   public function getName(): string
   {
     return $this->name;
@@ -22,5 +27,35 @@ class Rank
       throw new \InvalidArgumentException("Rank's name must be at least 3 characters long!");
     }
     $this->name = $name;
+  }
+
+  // Starting CRUD
+
+  public static function read(int $id): ?Rank
+  {
+    $statement = Database::getInstance()->getConnexion()->prepare('SELECT * FROM Rank WHERE id=:id;');
+    $statement->execute(['id' => $id]);
+    if ($row = $statement->fetch())
+      return new Rank(id: $row['id'], name: $row['name']);
+    return NULL;
+  }
+
+  public static function create(Rank $rank): string
+  {
+    $statement = Database::getInstance()->getConnexion()->prepare('INSERT INTO Rank (name) VALUES (:name);');
+    $statement->execute(['name' => $rank->getName()]);
+    return (string)Database::getInstance()->getConnexion()->lastInsertId();
+  }
+
+  public static function update(Rank $rank)
+  {
+    $statement = Database::getInstance()->getConnexion()->prepare('UPDATE Rank SET name=:name WHERE id=:id;');
+    $statement->execute((['name' => $rank->getName()]));
+  }
+
+  public static function delete(Rank $rank)
+  {
+    $statement = Database::getInstance()->getConnexion()->prepare('DELETE FROM Rank WHERE id=:id;');
+    $statement->execute(['id' => $rank->getId()]);
   }
 }
